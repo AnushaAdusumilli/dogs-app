903ea143b01a668259eebba2be9fd0080d4746b1
AnushaAdusumilli <anushaadusumilli1990@gmail.com>
2022-08-08 08:03:52 -0700

    Add DogHeader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DogComponent } from './dog/dog.component';
 import { DogsListComponent } from './dogs-list/dogs-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DogComponent,
     DogsListComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
